bb56660de39a76cdf475d7bfa4175a2e979f1d9a
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-02-16 13:33:54 +0200

    form actions

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,24 +1,46 @@
-export default function NewJokeRoute() {
-    return (
-      <div>
-        <p>Add your own hilarious joke</p>
-        <form method="post">
-          <div>
-            <label>
-              Name: <input type="text" name="name" />
-            </label>
-          </div>
-          <div>
-            <label>
-              Content: <textarea name="content" />
-            </label>
-          </div>
-          <div>
-            <button type="submit" className="button">
-              Add
-            </button>
-          </div>
-        </form>
-      </div>
-    );
+import {  redirect } from "remix";
+import type { ActionFunction} from "remix";
+import { db } from "~/utils/db.server";
+
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get('name');
+  const content = form.get('content');
+  if(typeof name !== 'string' || typeof content !== 'string'){
+    throw new Error('form data submitted is incorrect format, should be of type string');
   }
+
+  const joke = await db.joke.create({
+    data: {
+      name, content
+    }
+  });
+
+  return redirect(`/jokes/${joke.id}`);
+}
+
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <p>Add your own hilarious joke</p>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
